d5110cf75e3e00d56948fd013e6bfda03e93398b
bouthsandeep-lgtm <bouthsandeep@gmail.com>
2026-07-03 10:52:29 +0530

    StudentDetails.java

---
+++ StudentDetails.java
@@ -0,0 +1,18 @@
+class StudentDetails {
+    public static void main(String[] args) {
+        String studentName = "BAGUTHU SANDEEP";
+        String rollNumber = "25U31A0502";
+        String branch = "CSE";
+        char section = 'A';
+        double percentage = 85.5;
+        boolean hosteller = true;
+        
+        System.out.println("---- JNTUH R25 Student ----");
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll No: " + rollNumber);
+        System.out.println("Branch: " + branch);
+        System.out.println("Section: " + section);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Hosteller: " + hosteller);
+    }
+}
